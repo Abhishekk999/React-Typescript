85f9810ed86af7e49956e04bfc7f9c60a5025a15
Abhishekk999 <abhskushwaha@gmail.com>
2024-07-05 10:24:35 +0530

    Add some more cards

--- src/components/tasks/card.tsx
+++ src/components/tasks/card.tsx
@@ -45,25 +45,25 @@ const CardComponent = () => {
     {
       title: "Cityscape Twilight",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2016/11/30/16/33/cityscape-1900048_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/09/14/10/45/spaceship-5570682_640.jpg",
       description: "Twilight view of a city skyline.",
     },
     {
       title: "Landscape with Clouds",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2017/08/28/09/37/landscape-2621519_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/04/04/14/56/sunrise-8675236_640.jpg",
       description: "A serene landscape with fluffy clouds in the sky.",
     },
     {
       title: "Mountain Stream",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2015/10/14/17/42/mountain-stream-1037608_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/05/31/12/16/bridge-8800485_1280.jpg",
       description: "A crystal-clear stream flowing through a mountain valley.",
     },
     {
       title: "Sunset over the Bay",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2015/03/26/15/12/sunset-690968_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/06/21/15/18/road-8844555_1280.jpg",
       description: "A breathtaking sunset viewed from a bay.",
     },
   ];
